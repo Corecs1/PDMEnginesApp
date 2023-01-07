38fadec1e86cdb0fe06372899d5f9ee0e61d919b
Corecs <100100.98@mail.ru>
2023-01-07 18:06:51 +0400

    add some addEngine and addComponent methods

--- PDMEnginesApp/Form1.cs
+++ PDMEnginesApp/Form1.cs
@@ -2,6 +2,7 @@ using MySql.Data.MySqlClient;
 using PDMEnginesApp.config;
 using System.Data;
 using System.Xml.Linq;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
 
 namespace PDMEnginesApp
 {
@@ -17,8 +18,13 @@ namespace PDMEnginesApp
         {
             InitializeComponent();
             initializeData();
+
+            var root = treeView1.Nodes[0];
+            var collection = root.Nodes;
+            collection[0].Text = "gjkdjfghdfkjg";
         }
 
+        // Метод пробегается по базе данных и заполняет Node строками из БД
         private void initializeData()
         {
             DataTable table = new DataTable();
@@ -36,6 +42,7 @@ namespace PDMEnginesApp
             }
         }
 
+        // Метод пробегается по компонентам
         private void populateTreeView(int parentId, TreeNode parentNode)
         {
             DataTable table = new DataTable();
@@ -53,6 +60,8 @@ namespace PDMEnginesApp
             }
         }
 
+  
+        // Метод добавляет двигатель в БД и Node
         private void addEngine(object sender, EventArgs e)
         {
             DataTable table = new DataTable();
@@ -76,25 +85,82 @@ namespace PDMEnginesApp
             dataBase.closeConnection(); 
         }
 
+        // Метод добавляет компонент в БД и Node
         private void addComponent(object sender, EventArgs e)
         {
-            DataTable table = new DataTable();
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-            command = new MySqlCommand("INSERT INTO `pdm_engines`.`components` (name, amount) VALUES (@name, @amount);", dataBase.getConnection());
-            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = nameField.Text;
-            command.Parameters.Add("@amount", MySqlDbType.VarChar).Value = amountField.Text;
-            adapter.SelectCommand = command;
+            
+            /*
+                Проверка, добавлем ли мы компонент к двигателю или к компоненту
+                (Проверка содержится ли SelectedNode в RootNode)
+            */
+
+            if (treeView1.SelectedNode.Nodes != null)
+            {
+                var xui = treeView1.SelectedNode.FullPath;
+                String[] path = xui.Split('\\');
+
+                MessageBox.Show(path.Length.ToString());
+
+                foreach (String lol in path)
+                {
+                    MessageBox.Show(lol.ToString());
+                }
+
+     
+
+                DataTable table = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                command = new MySqlCommand("INSERT INTO `pdm_engines`.`components` (name, amount) VALUES (@name, @amount);", dataBase.getConnection());
+                command.Parameters.Add("@name", MySqlDbType.VarChar).Value = nameField.Text;
+                command.Parameters.Add("@amount", MySqlDbType.VarChar).Value = amountField.Text;
+                adapter.SelectCommand = command;
+
+                // SELECT id по параметрам name и amount двигателя из БД
+                command = new MySqlCommand("SELECT * FROM `pdm_engines`.`engines` WHERE `pdm_engines`.`engines`.name = @name", dataBase.getConnection());
+                command.Parameters.Add("@name", MySqlDbType.VarChar).Value = treeView1.SelectedNode.Nodes.ToString();
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                DataRow rows = table.Rows[0];
+                String engineId = rows["id"].ToString();
+
+                // SELECT id по параметрам name и amount компонента из БД
+                command = new MySqlCommand("SELECT * FROM `pdm_engines`.`components` WHERE `pdm_engines`.`components`.name = @name", dataBase.getConnection());
+                command.Parameters.Add("@name", MySqlDbType.VarChar).Value = nameField.Text;
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                DataRow rows1 = table.Rows[0];
+                String componentId = rows1["id"].ToString();
+
+                // INSERT INTO engine_component engine_id & component_id
+                command = new MySqlCommand("INSERT INTO `pdm_engines`.`engine_component` VALUES (@engine_id, @component_id);", dataBase.getConnection());
+                //command.Parameters.Add("@engine_id", MySqlDbType.Int64).Value;
+                command.Parameters.Add("@component_id", MySqlDbType.Int64).Value = componentId;
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+            }
+            else
+            {
+                DataTable table = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                command = new MySqlCommand("INSERT INTO `pdm_engines`.`components` (name, amount) VALUES (@name, @amount);", dataBase.getConnection());
+                command.Parameters.Add("@name", MySqlDbType.VarChar).Value = nameField.Text;
+                command.Parameters.Add("@amount", MySqlDbType.VarChar).Value = amountField.Text;
+                adapter.SelectCommand = command;
+                // SELECT id по параметрам name и amount компонента из БД
+                // INSERT INTO engine_component engine_id & component_id
+            }
 
             TreeNode node = new TreeNode(nameField.Text);
             try
             { 
-                treeView1.SelectedNode.Nodes.Add(node);
+                var xui = treeView1.SelectedNode.Nodes.Add(node);
             } catch (Exception ex) 
             { 
                 MessageBox.Show("Выберите строку для добавления компонента");
             }
         }
 
+        // Метод переименовывает двигатель или компонент
         private void rename(object sender, EventArgs e)
         {
             try
@@ -106,6 +172,7 @@ namespace PDMEnginesApp
             } 
         }
 
+        // Метод удаляет двигатель или компонент
         private void remove(object sender, EventArgs e)
         {
             try
